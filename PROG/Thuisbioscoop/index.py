e7a8fe6f80e17e5f7d21d90095f8b98f969067c6
Stefanve98 <stefan@van-echtelt.nl>
2018-10-13 13:05:56 +0200

    x

--- PROG/Thuisbioscoop/index.py
+++ PROG/Thuisbioscoop/index.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cecff03f1509d881852c2a9d84276214'
 
+
 # URL routes
 @app.route("/")
-def hello():
-    return render_template('home.html')  # load template
+def home():
+    return render_template('home.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
