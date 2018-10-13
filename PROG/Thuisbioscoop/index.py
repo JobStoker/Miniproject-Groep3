f5249da76c788ab6e0addc331943d491683e5724
Stefanve98 <stefan@van-echtelt.nl>
2018-10-13 13:14:44 +0200

    x

--- PROG/Thuisbioscoop/index.py
+++ PROG/Thuisbioscoop/index.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cecff03f1509d881852c2a9d84276214'
